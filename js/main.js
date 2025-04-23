62715bab5456f28b4290689911075530f073e0f4
Minh <minhphann215@gmail.com>
2025-04-23 15:29:32 +0700

    commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function renderUI(){
+    let rootDom = document.getElementById('root')
+    rootDom.style.width = '560px'
+    rootDom.style.height = '560px'
+    rootDom.style.backgroundColor = 'green'
+
+    rootDom.innerHTML = 'chao em'
+    
+}
+
+function change(){
+    renderUI()
+}
